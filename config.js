3c183b6cad3e859c11c205e3d12cbc34a96adc06
Zach Boudreaux <zach.boudreaux999@gmail.com>
2023-01-21 00:03:25 -0500

    update some file names

--- config.js
+++ config.js
@@ -3,17 +3,17 @@
 require("dotenv").config();
 require('colors');
 
-const SECRET_KEY = process.env.SECRET_KEY || 'capstone2';
+const SECRET_KEY = process.env.SECRET_KEY || 'foodly';
 const PORT = +process.env.PORT || 5000;
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === 'test' ? 1 : 12;
 
 const getDatabaseURI = () => {
     return (process.env.NODE_ENV === 'test')
-        ? 'capstone2_test' 
-        : process.env.DATABASE_URL || 'capstone2'
+        ? 'foodly_test' 
+        : process.env.DATABASE_URL || 'foodly'
 };
 
-console.log("Capstone2 Config:".green);
+console.log("Foodly Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT:".yellow, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
